a6a16b341b6ab8282bf48c38f1fcb12f5324d272
Arsen Nagdalian <nagoarsen@mail.ru>
2023-04-18 18:00:45 +0300

    Ask location permission

--- app/src/main/java/com/itmo/museum/MainActivity.kt
+++ app/src/main/java/com/itmo/museum/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.itmo.museum
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.itmo.museum.models.AppViewModel
 import com.itmo.museum.screens.MainScreen
 import com.itmo.museum.ui.theme.MuseumTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val viewModel: AppViewModel by viewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                viewModel.getDeviceLocation(fusedLocationProviderClient)
+            }
+        }
+
+    private fun askPermissions() = when {
+        ContextCompat.checkSelfPermission(
+            this,
+            ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED -> {
+            viewModel.getDeviceLocation(fusedLocationProviderClient)
+        }
+        else -> {
+            requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        askPermissions()
         setContent {
             MuseumTheme {
                 MainScreen()
--- app/src/main/java/com/itmo/museum/models/AppViewModel.kt
+++ app/src/main/java/com/itmo/museum/models/AppViewModel.kt
@@ -1,6 +1,9 @@
 package com.itmo.museum.models
 
+import android.location.Location
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,7 +12,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class AppViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(AppUiState(visitedMuseums = persistentListOf()))
+    private val _uiState = MutableStateFlow(
+        AppUiState(
+            lastKnownLocation = null,
+            visitedMuseums = persistentListOf()
+        )
+    )
     val uiState: StateFlow<AppUiState> = _uiState.asStateFlow()
 
     fun addVisitedMuseum(museum: Museum) {
@@ -19,8 +27,26 @@ class AppViewModel : ViewModel() {
             )
         }
     }
+
+    fun getDeviceLocation(
+        fusedLocationProviderClient: FusedLocationProviderClient
+    ) {
+        try {
+            val locationResult = fusedLocationProviderClient.lastLocation
+            locationResult.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _uiState.value = _uiState.value.copy(
+                        lastKnownLocation = task.result,
+                    )
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("loc", "Failed to get device location")
+        }
+    }
 }
 
 data class AppUiState(
+    val lastKnownLocation: Location?,
     val visitedMuseums: PersistentList<String>
 )
